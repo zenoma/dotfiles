3813a33cbae33c3a0d20ccbd49b762b1371a89f8
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-14 12:57:15 +0100

    Add vertical ruler

--- lua/options.lua
+++ lua/options.lua
@@ -56,4 +56,6 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Set color column
+vim.opt.colorcolumn = '80'
 -- vim: ts=2 sts=2 sw=2 et
