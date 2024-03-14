73f7e40c09031a8bb96995f9da1a5d24d98b8a75
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-14 11:46:08 +0100

    Change relative numbers

--- init.lua
+++ init.lua
@@ -91,7 +91,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
+
+-- Set to true to show relative numbers
+vim.wo.relativenumber = true
 
 -- [[ Setting options ]]
 require 'options'
