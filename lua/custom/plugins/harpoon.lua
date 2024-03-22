91f55d6872f82dc32b33474de52d2015aa92d591
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-22 11:19:26 +0100

    Revert "Try new config"
    This reverts commit 541423309f9655a48d6b0114b1af51f1674e88a3.

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,34 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+    ---@diagnostic disable-next-line: missing-parameter
+    harpoon:setup()
+    local function map(lhs, rhs, opts)
+      vim.keymap.set('n', lhs, rhs, opts or {})
+    end
+    map('<leader>a', function()
+      harpoon:list():append()
+    end, { desc = '[A]ppend file to Harpoon' })
+    map('<leader>r', function()
+      harpoon:list():remove()
+    end, { desc = '[R]emove file from Harpoon' })
+    map('<leader>h', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Show [H]arpoon' })
+    map('<leader>1', function()
+      harpoon:list():select(1)
+    end, { desc = 'Move to Harpoon view [1]' })
+    map('<leader>2', function()
+      harpoon:list():select(2)
+    end, { desc = 'Move to Harpoon view [2]' })
+    map('<leader>3', function()
+      harpoon:list():select(3)
+    end, { desc = 'Move to Harpoon view [3]' })
+    map('<leader>4', function()
+      harpoon:list():select(4)
+    end, { desc = 'Move to Harpoon view [4]' })
+  end,
+}
