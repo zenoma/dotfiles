1125bbb28a272b664de89f86647f46660b1060f0
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-04-11 14:29:56 +0200

    Add autopairs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -52,6 +52,8 @@ require('lazy').setup({
 
   require 'custom/plugins/trouble',
 
+  require 'custom/plugins/autopairs',
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- put them in the right spots if you want.
