2ce4b3a6ee16f53b0bb6f7806cd092081e2880f9
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-04-17 01:42:47 +0200

    Add new keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,6 +18,12 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagn
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Select all
+vim.keymap.set('n', '<C-a>', 'ggVG<cr>', { desc = 'Select all' })
+
+-- Clear search results
+vim.keymap.set('n', '<Esc>', '<cmd>noh<cr>')
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
