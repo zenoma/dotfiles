830262a5fa0da09f0045a3b74410d2cc49789f55
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-11-01 17:32:31 +0100

    Update theme

--- nvim/lua/user/plugins/theme.lua
+++ nvim/lua/user/plugins/theme.lua
@@ -1,80 +1,128 @@
-return {
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    priority = 1000,
-    config = function()
-      require("rose-pine").setup({
-        variant = "auto",      -- auto, main, moon, or dawn
-        dark_variant = "main", -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
+-- return {
+--   {
+--     'rose-pine/neovim',
+--     name = 'rose-pine',
+--     priority = 1000,
+--     config = function()
+--       require("rose-pine").setup({
+--         variant = "auto",      -- auto, main, moon, or dawn
+--         dark_variant = "main", -- main, moon, or dawn
+--         dim_inactive_windows = false,
+--         extend_background_behind_borders = true,
+--
+--         enable = {
+--           terminal = true,
+--           legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+--           migrations = true,        -- Handle deprecated options automatically
+--         },
+--
+--         styles = {
+--           bold = true,
+--           italic = true,
+--           transparency = false,
+--         },
+--
+--         groups = {
+--           border = "muted",
+--           link = "iris",
+--           panel = "surface",
+--
+--           error = "love",
+--           hint = "iris",
+--           info = "foam",
+--           note = "pine",
+--           todo = "rose",
+--           warn = "gold",
+--
+--           git_add = "foam",
+--           git_change = "rose",
+--           git_delete = "love",
+--           git_dirty = "rose",
+--           git_ignore = "muted",
+--           git_merge = "iris",
+--           git_rename = "pine",
+--           git_stage = "iris",
+--           git_text = "rose",
+--           git_untracked = "subtle",
+--
+--           h1 = "iris",
+--           h2 = "foam",
+--           h3 = "rose",
+--           h4 = "gold",
+--           h5 = "pine",
+--           h6 = "foam",
+--         },
+--
+--         highlight_groups = {
+--           TelescopeNormal = { fg = "subtle", bg = "base" },
+--           TelescopeSelection = { fg = "text", bg = "highlight_med" },
+--           TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
+--           TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
+--
+--           TelescopeTitle = { fg = "base", bg = "love" },
+--           TelescopePromptTitle = { fg = "base", bg = "pine" },
+--           TelescopePreviewTitle = { fg = "base", bg = "iris" },
+--
+--           TelescopePromptNormal = { fg = "text", bg = "base" },
+--
+--           Normal = { bg = 'base' },
+--           NormalFloat = { bg = 'base' },
+--         },
+--
+--       })
+--
+--       vim.cmd("colorscheme rose-pine")
+--
+--       -- vim.api.nvim_set_hl(0, 'normal', { bg = 'none' })
+--       -- vim.api.nvim_set_hl(0, 'normalfloat', { bg = 'none' })
+--     end,
+--   },
+-- }
+--
+--
+--
+--
+-- return {
+--   {
+--     'rose-pine/neovim',
+--     name = 'rose-pine',
+--     priority = 1000,
+--     config = function()
+--       require("rose-pine").setup({
+    --
+return { 
+    {
+        "ellisonleao/gruvbox.nvim",
+        name = "gruvbox",
+        priority = 1000,
+        config = function()
+            require("gruvbox").setup({
+              terminal_colors = true, -- add neovim terminal colors
+              undercurl = true,
+              underline = true,
+              bold = true,
+              italic = {
+                strings = true,
+                emphasis = true,
+                comments = true,
+                operators = false,
+                folds = true,
+              },
+              strikethrough = true,
+              invert_selection = false,
+              invert_signs = false,
+              invert_tabline = false,
+              invert_intend_guides = false,
+              inverse = true, -- invert background for search, diffs, statuslines and errors
+              contrast = "", -- can be "hard", "soft" or empty string
+              palette_overrides = {},
+              overrides = {},
+              dim_inactive = false,
+              transparent_mode = false,
+            })
+           vim.cmd.colorscheme "gruvbox"
+        end
 
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true,        -- Handle deprecated options automatically
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = false,
-        },
-
-        groups = {
-          border = "muted",
-          link = "iris",
-          panel = "surface",
-
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          note = "pine",
-          todo = "rose",
-          warn = "gold",
-
-          git_add = "foam",
-          git_change = "rose",
-          git_delete = "love",
-          git_dirty = "rose",
-          git_ignore = "muted",
-          git_merge = "iris",
-          git_rename = "pine",
-          git_stage = "iris",
-          git_text = "rose",
-          git_untracked = "subtle",
-
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
-
-        highlight_groups = {
-          TelescopeNormal = { fg = "subtle", bg = "base" },
-          TelescopeSelection = { fg = "text", bg = "highlight_med" },
-          TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-          TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
-
-          TelescopeTitle = { fg = "base", bg = "love" },
-          TelescopePromptTitle = { fg = "base", bg = "pine" },
-          TelescopePreviewTitle = { fg = "base", bg = "iris" },
-
-          TelescopePromptNormal = { fg = "text", bg = "base" },
-
-          Normal = { bg = 'base' },
-          NormalFloat = { bg = 'base' },
-        },
-
-      })
-
-      vim.cmd("colorscheme rose-pine")
-
-      -- vim.api.nvim_set_hl(0, 'normal', { bg = 'none' })
-      -- vim.api.nvim_set_hl(0, 'normalfloat', { bg = 'none' })
-    end,
-  },
+    }
 }
+-- setup must be called before loading
